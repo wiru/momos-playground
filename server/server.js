03366ef7a0a38d69c7784c68eede8320961f166d
Will Hauer <wiru_nihon@hotmail.com>
2021-05-05 02:11:18 +0900

    Unable to fix 2 errors/goals. memory leak. and showing player on other screen

--- server/server.js
+++ server/server.js
@@ -17,33 +17,35 @@ let activePlayers = {}
 // Run when client connects
 io.on('connection', socket => {
 	const id = uuid();
-	const player = {};
-	player.x = 50, // Horizontal position on x-axis
-	player.y = 50	, // Vertical position on y-axis
-	player.width = 32, // Our Sprite sheet width divided by 4 
-	player.height = 48, // Our Sprite sheet height divided by 4
-	player.frameX = 0, // A multiplier used later to find where to cut sprite from sprite sheet. 
-	player.frameY = 0, // Same for y. 
-	player.speed = 9, // How many pixels to move per frame.
-	player.moving = false // To know if we are stationary or moving 
-	console.log(player);
-	activePlayers[id] = player;
+	socket.emit('id', id, console.log("NewID"));
+	activePlayers[id] = {
+	x: 50, // Horizontal position on x-axis
+	y: 50	, // Vertical position on y-axis
+	width: 32, // Our Sprite sheet width divided by 4 
+	height: 48, // Our Sprite sheet height divided by 4
+	frameX: 0, // A multiplier used later to find where to cut sprite from sprite sheet. 
+	frameY: 0, // Same for y. 
+	speed: 9, // How many pixels to move per frame.
+	moving: false // To know if we are stationary or moving 
+	};
+	// console.log(activePlayers)
+	// console.log(id);
+	
+	setInterval(function(){
+		socket.emit('activePlayers', activePlayers, console.log("SEND")); 
+	}, 1000);
 
-	console.log(activePlayers);
-	socket.emit('id', id);
-	// socket.emit('message', 'Dis messig from saabaa!')
-	socket.on('player', playerObj => {
-		activePlayers.id = playerObj;
-		socket.emit('activePlayers', activePlayers);
-	});
+	// socket.emit('activePlayers', activePlayers, console.log("SEND"));
+	setInterval(function(){
+		socket.on('player', data => {
+			activePlayers[data.id] = data.data;
+			// console.log("SERVER", activePlayers)
+			// console.log(data.id)
+			});
+	}, 1000); 
 });
-
 
 const PORT = 4000 || process.env.PORT; 
 
 server.listen(PORT, ()=> console.log(`Hot Server Action on ${PORT}!`));
 
-
-
-
-// THE GAME //
