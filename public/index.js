3d840e1d0db6c4ff86fc07c02dd0f344947f22b1
Will Hauer <wiru_nihon@hotmail.com>
2021-05-05 14:17:47 +0900

    Extra Sprites and random

--- public/index.js
+++ public/index.js
@@ -39,8 +39,27 @@ window.addEventListener("keyup", function(e) { // This removes pressed key from 
 })
 ///////////////////////////////////////////////////////////////////////////////////////
 
+var sprites = [
+	"images/1.png",
+	"images/2.png",
+	"images/3.png",
+	"images/4.png",
+	"images/5.png",
+	"images/6.png",
+	"images/7.png",
+	"images/8.png",
+	"images/9.png",
+	"images/10.png",
+	"images/11.png",
+	"images/12.png",
+	"images/13.png",
+	"images/14.png",
+	"images/15.png",
+	"images/16.png",
+	];
+
 const playerSprite = new Image(); // Create new instance of Image Object
-playerSprite.src = "./images/hungary.png"; // Where to get the sprite sheet from. 
+playerSprite.src = getRandomSprite(); // Where to get the sprite sheet from. 
 
 const background = new Image();
 background.src = "./images/background.jpg";
@@ -48,6 +67,14 @@ background.src = "./images/background.jpg";
 let keys = [];
 
 let fps, fpsInterval, startTime, now, then, elapsed;
+
+///////////////////////////////////////////////////////////////////////////
+
+
+function getRandomSprite() {
+     var randomNum = Math.floor(Math.random() * sprites.length);
+     return sprites[randomNum];
+}
 
 //////////////////////////////////////////////////////////////
 
