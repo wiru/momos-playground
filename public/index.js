832adee547ef485fd24a8e78c586d78a8de5ea77
Will Hauer <wiru_nihon@hotmail.com>
2021-05-05 13:55:08 +0900

    LOOVE IT

--- public/index.js
+++ public/index.js
@@ -1,145 +1,134 @@
 const canvas = document.getElementById('main-canvas'); // Reference the canvas element from HTML.  
 const ctx = canvas.getContext('2d'); // Give access to built in canvas methods. 
+canvas.width = 800; // No px required
+canvas.height = 500;
+
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	  var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+	  return v.toString(16);
+	});
+  }
+  
+///////////////////////////////////////////////////////////////////////////////////////
 
 const socket = io(); 
 let activePlayers = {};
-let player = {};
-let uniqueId = "";
+let id = uuidv4();
+let player = {
+	id: id, 
+	x: 49,
+	y: 49,
+	width: 32,
+	height: 48,
+	frameX: 0,
+	frameY: 0,
+	speed: 9,
+	moving: false,
+};
+///////////////////////////////////////////////////////////////////////////////////////
+
+window.addEventListener("keydown", function(e) { // Catch key input. e is event. This is NOT the DOWN key. It means when a button is pushed down. 
+	keys[e.key] = true;
+	player.moving = true;	
+});
+
+window.addEventListener("keyup", function(e) { // This removes pressed key from array when released by the user.  
+	delete keys[e.key]
+	player.moving = false;
+})
+///////////////////////////////////////////////////////////////////////////////////////
+
+const playerSprite = new Image(); // Create new instance of Image Object
+playerSprite.src = "./images/hungary.png"; // Where to get the sprite sheet from. 
+
+const background = new Image();
+background.src = "./images/background.jpg";
+
+let keys = [];
+
+let fps, fpsInterval, startTime, now, then, elapsed;
+
+//////////////////////////////////////////////////////////////
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) { // this takes 9 arguments. sX etc is sprite sheet cutout size. dX etc is where to draw on the canvas.
 	ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 };
 
 ///////////////////////////////////////////////////////////////////////////
-socket.on('id', id => {
-	uniqueId = id
-});
 
-setInterval(function(){
 socket.on('activePlayers', playerObjs => {
 	activePlayers = playerObjs;
-	console.log("CLIENT", activePlayers)
-}, 1000); 
-	startAnimating(30);
-////////////////////////////////////////////////////////////////////////////
-})
+	// console.log("RECIEVE FROM SERVER", activePlayers)
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+function movePlayer() {
+
+	if (keys.w && player.y > 0) { 
+		// console.log("BEFORE", player.y)
+		player.y -= player.speed;
+		// console.log("AFTER", player.y)
+		player.frameY = 3;
+		player.moving = true;
+	}
+	if (keys.a && player.x > 0) {
+		player.x -= player.speed;
+		player.frameY = 1;
+		player.moving = true;
+	}
+	if (keys.s && player.y < 450) { //
+		player.y += player.speed;
+		player.frameY = 0;
+		player.moving = true;
+	}
+	if (keys.d && player.x < 765) {
+		player.x += player.speed;
+		player.frameY = 2;
+		player.moving = true;
+	}
+};
+
+function handlePlayerFrame() {
+	if (player.frameX < 3 && player.moving) player.frameX++
+	else player.frameX = 0
+}
+
 function startAnimating(fps) { 	// start animating based on the fps we pass in.
 	fpsInterval = 1000/fps; 	// 1000 milliseconds is a second. 1000 miliseconds / fps. e.g. 1000/30 = 33
 	then = Date.now(); // Gets a really long number of how many miliseconds since jan 1st.
 	startTime = then; // save this number. 
-	animate(); // run animate function. 
-	console.log(Date.now())
+	animate();
 }
 
-/////////////////////////////////////////////////////////////////////////////////////
 function animate() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear screen after every frame??. *thoughts* isn't this weird? Does it redraw background every time??
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height); // canvas has 3 methods. this is one of them. We use 5 arg short version here. Here we draw our background from 0,0 to width and height. 
-	for (const player in activePlayers) {
-		console.log(activePlayers)
-		drawSprite(playerSprite, 			// This is the player Image Object. Contains src of sprite sheet.   
-			activePlayers[player].width * activePlayers[player].frameX,  // Sprite height x multiplier. / TO CUT OUT FROM SPRITE SHEET. 	
-			activePlayers[player].height * activePlayers[player].frameY,	// Sprite height y multiplier. / TO CUT OUT FROM SPRITE SHEET.
-			activePlayers[player].width, 			// Sprite width. / TO CUT OUT FROM SPRITE SHEET.
-			activePlayers[player].height, 		// Sprite height. / TO CUT OUT FROM SPRITE SHEET.
-			activePlayers[player].x, 				// Player x position. / WHERE TO POSITION ON CANVAS.
-			activePlayers[player].y, 				// Player y position. / WHERE TO POSITION ON CANVAS.
-			activePlayers[player].width, 			// Sprite width. / WHERE TO POSITION ON CANVAS.
-			activePlayers[player].height 			// Sprite height. / WHERE TO POSITION ON CANVAS.
-			); // Takes sprite four corners from sprite sheet.
-
+	requestAnimationFrame(animate);
+	now = Date.now();
+	elapsed = now - then; // gets the difference in ms between last time checked and now.
+	if (elapsed > fpsInterval) { // if the elapsed time is greater than 
+		then = now - (elapsed % fpsInterval);
+		ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear screen after every frame??. *thoughts* isn't this weird? Does it redraw background every time??
+		ctx.drawImage(background, 0, 0, canvas.width, canvas.height); // canvas has 3 methods. this is one of them. We use 5 arg short version here. Here we draw our background from 0,0 to width and height. 
+		for (const player in activePlayers) {
+			// console.log(activePlayers[player])
+			drawSprite(playerSprite, 			// This is the player Image Object. Contains src of sprite sheet.   
+				activePlayers[player].width * activePlayers[player].frameX,  // Sprite height x multiplier. / TO CUT OUT FROM SPRITE SHEET. 	
+				activePlayers[player].height * activePlayers[player].frameY,	// Sprite height y multiplier. / TO CUT OUT FROM SPRITE SHEET.
+				activePlayers[player].width, 			// Sprite width. / TO CUT OUT FROM SPRITE SHEET.
+				activePlayers[player].height, 		// Sprite height. / TO CUT OUT FROM SPRITE SHEET.
+				activePlayers[player].x, 				// Player x position. / WHERE TO POSITION ON CANVAS.
+				activePlayers[player].y, 				// Player y position. / WHERE TO POSITION ON CANVAS.
+				activePlayers[player].width, 			// Sprite width. / WHERE TO POSITION ON CANVAS.
+				activePlayers[player].height 			// Sprite height. / WHERE TO POSITION ON CANVAS.
+				); // Takes sprite four corners from sprite sheet.
+				movePlayer();
+				handlePlayerFrame();
+				// console.log(activePlayers)
+			}
+	socket.emit("player", player)
 	}
-		// drawPlayers(activePlayers)
-		requestAnimationFrame(animate);
-		now = Date.now();
-		// console.log("CHECK2")
-		elapsed = now - then; // gets the difference in ms between last time checked and now.
-		if (elapsed > fpsInterval) { // if the elapsed time is greater than 
-			then = now - (elapsed % fpsInterval); //		
-			// console.log("CHECK")
-			movePlayer();
-			// console.log(player[uniqueId])
-			handlePlayerFrame();
-			setInterval(function(){
-				socket.emit('player', { data:activePlayers[uniqueId], id:uniqueId }, console.log("REPLY"))
-			}, 1000);
-			// console.log( uniqueId, player)
-		}
-	}
-	
-	
-	canvas.width = 800; // No px required
-	canvas.height = 500;
-	
-	const playerSprite = new Image(); // Create new instance of Image Object
-	playerSprite.src = "./images/hungary.png"; // Where to get the sprite sheet from. 
-	
-	const background = new Image();
-	background.src = "./images/background.jpg";
-	
-	
-	let keys = [];
-	
-	function movePlayer() {
-		// console.log(activePlayers);
-		// console.log(uniqueId)
-		if (keys.w && activePlayers[uniqueId].y > 0) { 
-			console.log("NORTH!")
-			console.log("BEFORE", activePlayers[uniqueId].y)
-			activePlayers[uniqueId].y -= activePlayers[uniqueId].speed;
-			console.log("AFTER", activePlayers[uniqueId].y)
-			activePlayers[uniqueId].frameY = 3;
-			activePlayers[uniqueId].moving = true;
-		}
-		if (keys.a && activePlayers[uniqueId].x > 0) {
-			activePlayers[uniqueId].x -= activePlayers[uniqueId].speed;
-			activePlayers[uniqueId].frameY = 1;
-			activePlayers[uniqueId].moving = true;
-		}
-		if (keys.s && activePlayers[uniqueId].y < 450) { //
-			activePlayers[uniqueId].y += activePlayers[uniqueId].speed;
-			activePlayers[uniqueId].frameY = 0;
-			activePlayers[uniqueId].moving = true;
-		}
-		if (keys.d && activePlayers[uniqueId].x < 765) {
-			activePlayers[uniqueId].x += activePlayers[uniqueId].speed;
-			activePlayers[uniqueId].frameY = 2;
-			activePlayers[uniqueId].moving = true;
-		}
-	};
-	
-	function handlePlayerFrame() {
-		// console.log(player[uniqueId].frameX)
-		if (activePlayers[uniqueId].frameX < 3 && activePlayers[uniqueId].moving) activePlayers[uniqueId].frameX++
-		else activePlayers[uniqueId].frameX = 0
-	}
-	
-	let fps, fpsInterval, startTime, now, then, elapsed;
-	
-	
-	// function drawPlayers(players) {
-		// 	// console.log(players)
-		// 	for (const player in players) {
-			// 		key = Object.keys(players[player]);
-			// 		// let p = players[player][key[0]];
-			// 		ctx.drawSprite(
-				// 			playerSprite, 			// This is the player Image Object. Contains src of sprite sheet.   
-				// 			players[player][key[0]].width * players[player][key[0]].frameX,  // Sprite height x multiplier. / TO CUT OUT FROM SPRITE SHEET. 	
-				// 			players[player][key[0]].height * players[player][key[0]].frameY,	// Sprite height y multiplier. / TO CUT OUT FROM SPRITE SHEET.
-				// 			players[player][key[0]].width, 			// Sprite width. / TO CUT OUT FROM SPRITE SHEET.
-				// 			players[player][key[0]].height, 		// Sprite height. / TO CUT OUT FROM SPRITE SHEET.
-				// 			players[player][key[0]].x, 				// Player x position. / WHERE TO POSITION ON CANVAS.
-				// 			players[player][key[0]].y, 				// Player y position. / WHERE TO POSITION ON CANVAS.
-				// 			players[player][key[0]].width, 			// Sprite width. / WHERE TO POSITION ON CANVAS.
-				// 			players[player][key[0]].height 			// Sprite height. / WHERE TO POSITION ON CANVAS.
-				// 			) // Takes sprite four corners from sprite sheet.
-				// 		}
-				// 	}
-				window.addEventListener("keydown", function(e) { // Catch key input. e is event. This is NOT the DOWN key. It means when a button is pushed down. 
-					keys[e.key] = true;
-					// console.log(keys)
-				});
-				window.addEventListener("keyup", function(e) { // This removes pressed key from array when released by the user.  
-					delete keys[e.key]
-					activePlayers[uniqueId].moving = false;
-				});		
+}
+startAnimating(30);
+		
--- server/server.js
+++ server/server.js
@@ -16,36 +16,13 @@ let activePlayers = {}
 
 // Run when client connects
 io.on('connection', socket => {
-	const id = uuid();
-	socket.emit('id', id, console.log("NewID"));
-	activePlayers[id] = {
-	x: 50, // Horizontal position on x-axis
-	y: 50	, // Vertical position on y-axis
-	width: 32, // Our Sprite sheet width divided by 4 
-	height: 48, // Our Sprite sheet height divided by 4
-	frameX: 0, // A multiplier used later to find where to cut sprite from sprite sheet. 
-	frameY: 0, // Same for y. 
-	speed: 9, // How many pixels to move per frame.
-	moving: false // To know if we are stationary or moving 
-	};
-	// console.log(activePlayers)
-	// console.log(id);
-	
-	setInterval(function(){
-		socket.emit('activePlayers', activePlayers, console.log("SEND")); 
-	}, 1000);
-
-	// socket.emit('activePlayers', activePlayers, console.log("SEND"));
-	setInterval(function(){
-		socket.on('player', data => {
-			activePlayers[data.id] = data.data;
-			// console.log("SERVER", activePlayers)
-			// console.log(data.id)
-			});
-	}, 1000); 
+	socket.on('player', player => {
+		activePlayers[player.id] = player
+		socket.emit('activePlayers', activePlayers); 
+	});
 });
 
-const PORT = 4000 || process.env.PORT; 
+const PORT = process.env.PORT || 4000; 
 
 server.listen(PORT, ()=> console.log(`Hot Server Action on ${PORT}!`));
 
